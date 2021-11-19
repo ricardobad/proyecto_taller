421c2cc1f0b328a9c247754e66a82bc1742eba20
MBELENMORA <91024534+MBELENMORA@users.noreply.github.com>
2021-11-18 22:54:26 -0600

    Test Servicio

--- proyecto_taller-dev/Proyecto/Proyecto1/src/ServicioTest.java
+++ proyecto_taller-dev/Proyecto/Proyecto1/src/ServicioTest.java
@@ -1,7 +1,12 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+public class ServicioTest {
+    @Test
+    public void testRevision(){
+        assertEquals(0, 0);
 
+    }
 
 
 }
