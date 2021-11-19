2265af84a329f4ca80196b6cbe9300f54f36ae62
ricardobad <90865669+ricardobad@users.noreply.github.com>
2021-11-18 22:55:55 -0600

    Agrega prueba unitaria

--- proyecto_taller-dev/Proyecto/Proyecto1/src/ServicioTest.java
+++ proyecto_taller-dev/Proyecto/Proyecto1/src/ServicioTest.java
@@ -1,12 +1,19 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+
 public class ServicioTest {
     @Test
     public void testRevision(){
         assertEquals(0, 0);
 
     }
+    @Test
+    public void testMantenimiento(){
+        assertEquals(0, 0);
+    }
+    
+  
 
 
 }
